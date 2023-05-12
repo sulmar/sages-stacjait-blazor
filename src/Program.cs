eee12085b101a5548cd32173cc578f6bd8f5bdad
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-05-12 10:59:17 +0200

    Pobieranie użytkowników

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using StacjaIT.BlazorApp;
+using StacjaIT.BlazorApp.Abstractions;
+using StacjaIT.BlazorApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// dotnet add package Microsoft.Extensions.Http
+builder.Services.AddHttpClient<IUserService, JsonPlaceHolderUserService>(sp => sp.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/"));
 
 await builder.Build().RunAsync();
---
+++ src/Services/JsonPlaceHolderUserService.cs
@@ -0,0 +1,12 @@
+﻿using StacjaIT.BlazorApp.Abstractions;
+using StacjaIT.BlazorApp.Models;
+using System.Net.Http.Json;
+
+namespace StacjaIT.BlazorApp.Services;
+
+public class JsonPlaceHolderUserService : IUserService
+{
+    private readonly HttpClient http;
+    public JsonPlaceHolderUserService(HttpClient http) => this.http = http;
+    public Task<List<User>> GetAllAsync() => http.GetFromJsonAsync<List<User>>("/users");
+}
