babd79c02c2f22c0246c7e5938a24ae7f0cec2ba
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-05-12 10:43:47 +0200

    Init

---
+++ src/Abstractions/IUserService.cs
@@ -0,0 +1,9 @@
+﻿
+using StacjaIT.BlazorApp.Models;
+
+namespace StacjaIT.BlazorApp.Abstractions;
+
+public interface IUserService
+{
+    Task<List<User>> GetAllAsync();
+}
--- src/README.md
+++ src/README.md
@@ -1,1 +1,14 @@
 # Blazor
+
+
+~~~ bash
+dotnet new blazorwasm-empty -o StacjaIT.BlazorApp
+~~~ 
+
+~~~ bash
+cd StacjaIT.BlazorApp
+dotnet run
+~~~ 
+
+
+
